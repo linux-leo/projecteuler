cdf47be53ad30a199fc284c62198c6a55a35a58d
linux-leo <leo@catsconsultants.com>
2024-08-24 18:11:28 +0000

    refactor

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 print(sum(filter(lambda i: not (i % 3 and i % 5), range(1000))))
 
+class Fibonacci:
+    def fib(self):
+        a, b = 0, 1
+        def next_fib():
+            nonlocal a, b
+            a, b = b, a + b
+            return a
+        return next_fib
 
-def sum_even_fibs_loop(limit):
-    sum_even = 0
-    i, j = 1, 1
-    while j < limit:
-        sum_even += i*(1-(i % 2))
-        i, j = i + j, i
-    return sum_even
+f = Fibonacci().fib()
+even_sum = 0
+fib_number = 0
 
+while fib_number < 4_000_000:
+    if not (fib_number := f()) % 2:
+        even_sum += fib_number
 
-print(sum_even_fibs_loop(4000000))
+print(even_sum)
