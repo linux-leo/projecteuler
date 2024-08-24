881d88ef73e386745e34eab7b11ada095d998953
linux-leo <leo@catsconsultants.com>
2024-08-24 20:11:52 +0000

    Problem 3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 print(sum(filter(lambda i: not (i % 3 and i % 5), range(1000))))
 
 class Fibonacci:
@@ -18,3 +20,16 @@ while fib_number < 4_000_000:
         even_sum += fib_number
 
 print(even_sum)
+
+n,p = 600851475143,1
+while p < n:
+    n //= p
+    i, p = 2, n
+    while n % i:
+        if i > math.isqrt(n)+1:
+            break
+        i += 1
+    else:
+        p = i
+
+print(n)
